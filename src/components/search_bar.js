e30b06e4b51ed46d514ba585fad419539fa63741
Brice Djilo <dbrice@uno.edu>
2017-08-26 21:31:21 -0600

    Adds search function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { term: 'Starting value' };
+        this.state = { term: 'search' };
     }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+
     render() {
         return (
             <div className="form-group search-bar">
                 <input type="text" className="form-control" value={this.state.term}
-                onChange={event => this.setState({term: event.target.value})} />
+                onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
     }
