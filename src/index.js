bd8fab2c9cac0adddb1348d56deac6d778cb1817
Brice Djilo <dbrice@uno.edu>
2017-08-26 18:07:10 -0600

    Adds video list and details

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBHsLIWHT8jMD6FCVaeh8P0PKGAc-s3iRQ';
 
-YTSearch({key: API_KEY, term: 'game of throne'}, function(data) {
-    console.log(data);
-});
-
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'game of throne'}, videos => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component generated HTML and put it on the page
